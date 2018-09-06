bad194e52aee807086918445a0a5fc2493a5ad9b
Florian Kolly <fkollygoogle@hotmail.fr>
2018-09-06 13:07:45 +0200

    Add geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,5 +10,18 @@ describe('generateMessage', () => {
 
     expect(message).toInclude({from, text});
     expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should genenrate correct location object', () => {
+    var from = 'Test';
+    var lat = 1;
+    var lng = 1;
+    var url = `https://www.google.com/maps?q=${lat},${lng}`;
+    var location = generateLocationMessage(from, lat, lng);
+
+    expect(location.createdAt).toBeA('number');
+    expect(location).toInclude({from, url});
   });
 });
